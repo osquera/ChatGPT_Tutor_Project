36995df84f19e51ccfe1f178974d4e4d44ec24ef
Anton <s214611@dtu.dk>
2023-06-08 11:12:46 +0200

    Small fixes and a test comparison between the pipeline and sole ChatGPT

--- entire_pipeline.py
+++ entire_pipeline.py
@@ -1,3 +1,5 @@
+from typing import Tuple, Any
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,8 +7,9 @@ import matplotlib.pyplot as plt
 from transformers import AutoTokenizer, AutoModel
 import torch
 
-#for not seing a warning message
+# for not seeing a warning message
 import logging
+
 logging.getLogger("transformers.modeling_utils").setLevel(logging.ERROR)
 
 from keras.models import model_from_json
@@ -14,12 +17,12 @@ from keras.models import model_from_json
 import openai
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
-
-def pipeline(query:str, ) -> str:
+def pipeline(query: str, method: str = 'cs') -> str:
     """
     This function is the pipeline for the entire project. It takes in a query and finds the most relevant document.
     and gives it to the OpenAI API to generate a answer
@@ -30,11 +33,12 @@ def pipeline(query:str, ) -> str:
     # 1. Preprocess the query
     embedding = get_text_embedding(query)
     # 2. Semantic Search
-    best_ctx = semantic_search_model(embedding, method='ann')
+    best_ctx = semantic_search_model(embedding, method)
     # 3. Answer Generation
     answer = answer_generation(query, best_ctx)
     # 4. Return the answer
     return answer
+
 
 def get_text_embedding(text, model_name='bert-base-uncased'):
     # Load pre-trained model and tokenizer
@@ -53,11 +57,12 @@ def get_text_embedding(text, model_name='bert-base-uncased'):
 
     return embedding
 
-def semantic_search_model(embedding:np.ndarray, method:str = 'ann') -> str:
+
+def semantic_search_model(embedding: np.ndarray, method: str = 'ann') -> str:
     """
     This function takes in a query embedding and finds the most relevant document by using the ANN
-    :param embedding:
-    :return:
+    :param embedding: The query embedding of the query of dimension 768
+    :return: The most relevant 2 most relevant contexts
     """
     # load the dataset
     df = pd.read_pickle('./Data_Generation/df_pickle/question-context-20-emb.pkl')
@@ -85,7 +90,26 @@ def semantic_search_model(embedding:np.ndarray, method:str = 'ann') -> str:
         # Get the context
         context1 = df.iloc[index[0][0]]['context']
         context2 = df.iloc[index[1][0]]['context']
-        context = context1+ context2
+        context = context1 + context2
+
+    if method == 'weighted_cs':
+        with open('model_cos.json', 'r') as json_file:
+            loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        # load weights into new model
+        loaded_model.load_weights("model_cos.h5")
+        print("Loaded model from disk")
+        # Predict the most relevant context
+        prediction = loaded_model.predict(model_input)
+        # Get the 2 most relevant contexts
+        index = np.argsort(prediction, axis=0)[-2:]
+        # Get the context
+        context1 = df.iloc[index[0][0]]['context']
+        context2 = df.iloc[index[1][0]]['context']
+        context = context1 + context2
+
+
     elif method == 'cs':
         # Should simply be the cosine similarity
         x = np.array(embedding)
@@ -100,31 +124,50 @@ def semantic_search_model(embedding:np.ndarray, method:str = 'ann') -> str:
 
     return context
 
-def answer_generation(query:str, context:str) -> str:
+
+def answer_generation(query: str, context: str = "", pipeline_mode=True) -> tuple[str, str] | Any:
     """
     This function takes in a query and a context and uses the OpenAI API to generate an answer
     :param query:
     :param context:
     :return:
     """
-    print(f'CONTEXT: ```{context}``` QUESTION: ```{query}``` ANSWER:')
-    try:
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            temperature=0,
-            messages=[
-                {"role": "system",
-                 "content": "You are a Teachers Assistant and you should answer the QUESTION using the information given in the CONTEXT, if the CONTEXT is unrelated, you should ignore it."},
-                {"role": "user", "content": f'CONTEXT: ```{context}``` QUESTION: ```{query}``` ANSWER:'},
-            ]
-        )
-    except Exception as e:
-        print("OPENAI_ERROR:", str(e))
+    if pipeline_mode:
+        print(f'CONTEXT: ```{context}``` QUESTION: ```{query}``` ANSWER:')
+        try:
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                temperature=0,
+                messages=[
+                    {"role": "system",
+                     "content": "You are a Teachers Assistant and you should answer the QUESTION using the information given in the CONTEXT, if the CONTEXT is unrelated, you should ignore it."},
+                    {"role": "user", "content": f'CONTEXT: ```{context}``` QUESTION: ```{query}``` ANSWER:'},
+                ]
+            )
+        except Exception as e:
+            return "OPENAI_ERROR:", str(e)
+
+
+    else:
+        try:
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                temperature=0,
+                messages=[
+                    {"role": "user", "content": f'QUESTION: ```{query}``` ANSWER:'},
+                    ]
+
+            )
+        except Exception as e:
+            return "OPENAI_ERROR:", str(e)
 
     return completion.choices[0]
 
 
 if __name__ == "__main__":
     # Read in the data
-    answer = pipeline("Which linkage function uses the eucladian distance? Does both maximum and average linkage use the eucladian distance?")
-    print(answer)
+    query = "Which linkage function uses the eucladian distance? Does both maximum and average linkage use the eucladian distance?"
+    answer_pipeline = pipeline(query, method='cs')
+    answer_chatgpt = answer_generation(query, pipeline_mode=False)
+    print(answer_pipeline)
+    print(answer_chatgpt)
